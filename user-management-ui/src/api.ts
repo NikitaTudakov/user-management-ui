6dda779db75a5d37bacfbfa55d079055b9676715
NikitaTudakov <tudakov.nikita@gmail.com>
2023-09-12 19:04:50 +0300

    basic app done

---
+++ user-management-ui/src/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000'; // Замените на URL вашего API
+
+export const getUsers = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/users`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+};
